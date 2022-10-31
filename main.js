a48473a220784f33803fa0c14748fcfe09067ede
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-10-31 12:55:31 +0700

    hjg

--- main.js
+++ main.js
@@ -10,36 +10,46 @@ function makeSchoolchild(name, age, gender, weight, growth) {
 
 let schoolchildren = [];
 
-function createObjects(func) {
-    alert('Введите значения')
-    let name = prompt('Имя');
-    let age = prompt('Возраст');
-    let gender = prompt('Пол');
-    let weight = prompt('Вес');
-    let growth = prompt('Рост');
-    let schoolchild = [
-        name,
-        age,
-        gender,
-        weight,
-        growth
-    ]
+function createObjects() {
+    if(confirm('Введите значения')){
+        let name = prompt('Имя');
+        let age = prompt('Возраст');
+        let gender = prompt('Пол');
+        let weight = prompt('Вес');
+        let growth = prompt('Рост');
+        let schoolchild = [
+            name,
+            age,
+            gender,
+            weight,
+            growth
+        ]
 
-    schoolchildren += [JSON.stringify(schoolchild.join(', '))]
-    return schoolchildren
+        schoolchildren += [schoolchild.join(', ') + ', ']
+        viewSchoolchild()
+        return schoolchildren
+    }
+    else searchOutputValues()
 }
 
-function viewSchoolchild(...item) {
-    let isTrue = confirm('Посмотреть данные')
-    if (isTrue) {
-        let schoolchild = schoolchildren.slice(0, schoolchildren.length)
-        alert(schoolchild)
+function viewSchoolchild() {
+    if (confirm('Посмотреть данные')) {
+        alert(schoolchildren)
+        addSchoolchild()
+    }
+    else addSchoolchild()
+}
+
+function addSchoolchild() {
+   if (confirm('Добавить объект школьника')) createObjects()
+    else searchOutputValues()
+}
+
+function searchOutputValues() {
+    if(confirm('Поиск данных школьника')) {
+        let indexName = schoolchildren.indexOf(prompt('Имя'))
+        alert(schoolchildren.slice(indexName, indexName+15))
     }
 }
 
-function methods() {
-    createObjects(makeSchoolchild)
-    viewSchoolchild()
-}
-
-methods()
+createObjects()
